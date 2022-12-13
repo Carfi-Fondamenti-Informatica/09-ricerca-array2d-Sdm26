250d1d12f3444dc0f757aaccb797651775375f70
Sdm26 <118051850+Sdm26@users.noreply.github.com>
2022-12-13 01:10:01 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,15 +1,15 @@
 #include "lib.h"
-using namespace std;
-bool prod(char num[10][20],int &posi,char tit[20]){
-    for (int i=0;i<10;i++){
-        for(int b=0;b<20;b++){
-            if(num[i][b]!=tit[b]){
-                b=19;
-            }else {
-                if (b == 19) {
-                    posi = i;
-                    return true;
-                }
+
+bool cerca(char lista[10][20], int &posizione, char target[]){
+    for(int i=0;i<10;i++){
+        for(int j=0;j<20;j++){
+            if(lista[i][j] != target[j]){
+                j = 20;
+            }else{
+              if(j==19){
+                  posizione = i;
+                  return true;
+              }
             }
         }
     }
