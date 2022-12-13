6fed6728f3fd1951c98abfe7824a1be40b9e6bd6
Sdm26 <118051850+Sdm26@users.noreply.github.com>
2022-12-13 01:05:45 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
-
+#include "lib.h"
+using namespace std;
+bool prod(char num[10][20],int &posi,char tit[20]){
+    for (int i=0;i<10;i++){
+        for(int b=0;b<20;b++){
+            if(num[i][b]!=tit[b]){
+                b=19;
+            }else {
+                if (b == 19) {
+                    posi = i;
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
