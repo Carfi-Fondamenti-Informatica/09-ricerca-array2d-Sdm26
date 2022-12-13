5d4d68bb2757337ba25c5c5ec0d579f6a8fa4f59
Sdm26 <118051850+Sdm26@users.noreply.github.com>
2022-12-13 01:06:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+using namespace std;
+int main() {
+    char num[10][20];
+    char tit[20];
+    int pos=0;
+    for(int i=0;i<10;i++){
+        for(int b=0;b<20;b++){
+            num[i][b]=' ';
+            tit[b]=' ';
+        }
+    }
+    for(int i=0;i<10;i++){
+        cin>>num[i];
+    }
+   cin>>tit;
+if(prod(num,pos,tit)){
+cout<<pos;
+}else{
+    cout<<"non presente";
+}
+    return 0;
 }
